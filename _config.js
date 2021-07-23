181ce4977ab1902472641f6ffc07db4d39314998
DAEUN <kdw971203@naver.com>
2021-07-23 14:40:56 +0900

    Update

--- _config.js
+++ _config.js
@@ -27,7 +27,7 @@ module.exports = {
 
   /** Post */
   enablePostOfContents: true,     // TableOfContents activation (Type of Value: Boolean. Not String)
-  disqusShortname: '',            // comments (Disqus sort-name)
+  disqusShortname: 'junhobaik',            // comments (Disqus sort-name)
   enableSocialShare: true,        // Social share icon activation (Type of Value: Boolean. Not String)
 
   /** Optional */
